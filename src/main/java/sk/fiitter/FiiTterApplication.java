4f27e0366d372c7f698a64a7c6f8f29cd8bc0465
vrusi <v.rusinkova97@gmail.com>
2020-05-15 13:25:19 +0200

    Initial commit

---
+++ src/main/java/sk/fiitter/FiiTterApplication.java
@@ -0,0 +1,13 @@
+package sk.fiitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiiTterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiiTterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/fiitter/FiiTterApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.fiitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiiTterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
