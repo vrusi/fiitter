a1ea2447eff1d62ae8c51263bf671ae72773fa73
vrusi <v.rusinkova97@gmail.com>
2020-05-15 13:31:34 +0200

    Fix typo in class name

--- src/main/java/sk/fiitter/FIITterApplication.java
+++ src/main/java/sk/fiitter/FIITterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FiiTterApplication {
+public class FIITterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FiiTterApplication.class, args);
+		SpringApplication.run(FIITterApplication.class, args);
 	}
 
 }
