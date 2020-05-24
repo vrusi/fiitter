74c46c4d7e2f89d67115b6f3e0766a451e281790
vrusi <v.rusinkova97@gmail.com>
2020-05-24 23:00:48 +0200

    change home.html to index.html

---
+++ src/main/java/sk/fiitter/ErrorControllerImpl.java
@@ -0,0 +1,39 @@
+package sk.fiitter;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+public class ErrorControllerImpl implements ErrorController {
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        // get error status
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        // TODO: log error details here
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            // display specific error page
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "500";
+            } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "403";
+            }
+        }
+
+        // display generic error
+        return "error";
+    }
+}
